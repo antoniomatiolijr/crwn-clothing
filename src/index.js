a26efc29b4466a116e3c5c2f9b2fcb1967199dcd
antoniomatiolijr <antoniomatiolijr@gmail.com>
2020-04-20 19:50:36 -0300

    add redux persist to our store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { store, persistor } from "./redux/store";
+import App from "./App";
+
+import "./index.css";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import cartReducer from "./cart/cart.reducer";
 import userReducer from "./user/user.reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"], // we specify the chosen reducers, and this case user is handle by firestore
+};
+
+const rootReducer = combineReducers({
   cart: cartReducer,
   user: userReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
