778a9bcdc0be2fe3b39706befd65a603f224ae3c
antoniomatiolijr <antoniomatiolijr@gmail.com>
2020-05-02 19:11:29 -0300

    integrate with stripe payment api

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,38 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_IJxSVxVUa5LuLohOtAJPq3Oj00HyhWgLDB";
 
   const onToken = (token) => {
-    console.log(token);
+    let url;
+
+    if (process.env.NODE_ENV === "development") {
+      url = "http://localhost:5000";
+    } else {
+      url = "https://matioli-crwn-clothing-api.herokuapp.com";
+    }
+
+    axios({
+      url: `${url}/payment`,
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment successful");
+      })
+      .catch((error) => {
+        debugger;
+        console.log("Payment error: ", error);
+        alert(
+          "there is an issue with your payment. Please sure you use the provided credit cart."
+        );
+      });
   };
 
   return (
