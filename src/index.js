6805cb72560602f1694c355462a6c1a83de2e823
antoniomatiolijr <antoniomatiolijr@gmail.com>
2020-05-12 09:26:41 -0300

    convert app to pwa

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
 import { store, persistor } from "./redux/store";
+import * as serviceWorker from "./serviceWorker";
+
 import App from "./App";
 
 import "./index.css";
@@ -19,3 +21,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+serviceWorker.register();
